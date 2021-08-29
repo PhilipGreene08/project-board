ea9b01e3b397091d470947e295672a1191f7927a
Phil Greene <philgreene08@gmail.com>
2021-08-29 16:43:19 -0500

    updated renderTodos andsaved items

--- dist/app.js
+++ dist/app.js
@@ -7,15 +7,46 @@ const deleteAllButton = document.querySelector(`.delete-all-button`)
 
 form.addEventListener('submit', addToDo)
 deleteAllButton.addEventListener(`click`, deleteAllFunction)
+ul.addEventListener(`click`, deleteItem)
 
 let masterTodoList = []
 
-ul.addEventListener(`click`, deleteItem)
+window.onload = function () {
+    renderTodos()
+}
+
+function addToDo(event) {
+    event.preventDefault()
+    masterTodoList.push(newTodo.value)
+    saveToLocalStorage(newTodo.value)
+    newTodo.value = ``
+    renderTodos()
+}
+
+function renderTodos() {
+    masterTodoList = JSON.parse(localStorage.getItem('Full List')) || [] //need to keep this line or they wont render onload
+    console.log(masterTodoList);
+    if (masterTodoList.length > 0) {
+        console.log(`yes`);
+        ul.innerHTML = ``
+        masterTodoList.forEach(todo => {
+            let indexNumber = masterTodoList.indexOf(`${todo}`)
+            const li = document.createElement(`li`)
+            li.setAttribute('id', `${indexNumber}`)
+            li.classList.add(`todo`)
+            li.innerHTML = `<p>${todo}</p> <i class="fas fa-trash"></i> <i class="far fa-edit"></i>`
+            ul.appendChild(li)
+        });
+    } else {
+        console.log(`no data`);
+    }
+}
 
 function deleteItem() {
     let clicked = event.target
+    console.log(event.target.parentElement);
+    console.log(clicked);
     if (clicked.classList.contains(`fa-trash`)) {
-        console.log(`yes`);
         let selectedNode = clicked.parentElement
         selectedNode.parentElement.removeChild(selectedNode)
     } else {
@@ -23,48 +54,15 @@ function deleteItem() {
     }
 }
 
-//onload, render todos list
-renderTodos(masterTodoList)
-
-function addToDo(event) {
-    event.preventDefault()
-    masterTodoList.push(newTodo.value)
-    newTodo.value = ``
-    saveToLocalStorage()
-    renderTodos(masterTodoList)
-
-}
-
-function renderTodos(masterTodoList) {
-    if (masterTodoList.length > 0) {
-        //clear screen of all todos and empty the masterToDoList to avoid double todos
-        ul.innerHTML = ``
-        masterTodoList = []
-
-        // get items from LS
-        let storedItems = JSON.parse(localStorage.getItem(`Full List`))
-        masterTodoList.push(storedItems)
-        // for each todo, create element and render in DOM
-        storedItems.forEach(todo => {
-            const li = document.createElement(`li`)
-            li.classList.add(`todo`)
-            li.innerHTML = `<p>${todo}</p> <i class="fas fa-trash"></i> <i class="far fa-edit"></i>`
-            ul.appendChild(li)
-        })
-
-    } else {
-        console.log(`no data`);
-    }
-}
-
-
-
 function deleteAllFunction() {
     ul.innerHTML = ``
     localStorage.clear()
 }
 
 // save list to LS
-function saveToLocalStorage() {
+function saveToLocalStorage(data) {
+    let masterTodoList = []
+    masterTodoList = JSON.parse(localStorage.getItem('Full List')) || []
+    masterTodoList.push(data)
     localStorage.setItem(`Full List`, JSON.stringify(masterTodoList))
 }
